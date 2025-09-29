23791917c078d099039bb4448e84a67c577090e0
Kazim Abbas Bhatti <108944727+KazimAbbas110@users.noreply.github.com>
2025-09-29 11:18:37 -0700

    Update ghlengine-y.js

--- ghlengine-y.js
+++ ghlengine-y.js
@@ -1,14 +1,16 @@
-// ghl-dashboard-v2.js
 (function() {
   const css = `
-  .transition-slowest .flex-col > .overflow-hidden {
-    background: linear-gradient(135deg, #f7971e 0%, #ffd200 100%);
-}
+ 
+/* yellow Background Only */
 
-;
-  const style = document.createElement('style');
+
+.transition-slowest .flex-col > .overflow-hidden {
+ background: linear-gradient(135deg, #f7971e 0%, #ffd200 100%);
+
+}
+  `;
+
+  const style = document.createElement("style");
   style.textContent = css;
   document.head.appendChild(style);
 })();
-
-
